bb42ee24a22dad4f74638eaefb6d8db64e760d18
Franklin <franklinbarbs763@gmail.com>
2024-11-04 19:14:11 -0300

    feat(company-info): should add back return btn

--- src/screens/company-infos/company-infos.tsx
+++ src/screens/company-infos/company-infos.tsx
@@ -1,17 +1,22 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import { RecursiveElement } from './components/recursive-element';
 import { getFilteredChildrenStart } from './helpers';
 import { useAllElements } from './hooks/useAllElements';
+import backBtnIcon from '../../assets/images/return.svg';
 import styles from './styles.module.css';
+import { PAINEL_PATHS } from '../../helpers/painel-paths';
 
 export const CompanyInfos = () => {
-
-    const allElements = useAllElements("662fd0ee639069143a8fc387");
-
+    const navigate = useNavigate();
+    const { companyName = '-', id = '' } = useParams();
+    const allElements = useAllElements(id);
     const filteredElements = getFilteredChildrenStart(allElements);
 
     return <div className={styles.companyInfos}>
-        Company Infos
-        <br />
+        <h2>
+            <img src={backBtnIcon} alt='back-btn' onClick={() => navigate(PAINEL_PATHS.companies.path)} />
+            {companyName}
+        </h2>
         {
             filteredElements.map(element => (
                 <RecursiveElement 
