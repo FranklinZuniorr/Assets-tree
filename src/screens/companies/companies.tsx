14313643f5cdd95ce519f887037d2ceb5b0e4a79
Franklin <franklinbarbs763@gmail.com>
2024-11-05 10:18:30 -0300

    feat(companies): should add more info

--- src/screens/companies/companies.tsx
+++ src/screens/companies/companies.tsx
@@ -12,15 +12,18 @@ export const Companies = () => {
         <div className={styles.companies}>
             <h2>All available companies</h2>
             <div className={styles.companies_content}>
-                {
-                    companies?.map(company => (
-                        <CardCompany 
-                        key={company.id} 
-                        companyName={company.name} 
-                        onClick={() => navigate(PAINEL_PATHS.companyInfo.fnPath(company.id, company.name))} 
-                        />
-                    ))
-                }
+                <p>Here you find all assets, components and locations of some company</p>
+                <div className={styles.companies_content_renderArea}>
+                    {
+                        companies?.map(company => (
+                            <CardCompany 
+                            key={company.id} 
+                            companyName={company.name} 
+                            onClick={() => navigate(PAINEL_PATHS.companyInfo.fnPath(company.id, company.name))} 
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
