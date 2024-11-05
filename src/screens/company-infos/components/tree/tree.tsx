d112109a755e56d590bc7625c7644432e5958388
Franklin <franklinbarbs763@gmail.com>
2024-11-05 09:53:29 -0300

    style(tree): should change filter area styles

--- src/screens/company-infos/components/tree/tree.tsx
+++ src/screens/company-infos/components/tree/tree.tsx
@@ -4,7 +4,11 @@ import { useAllElements } from '../../hooks/useAllElements';
 import { RecursiveElement } from '../recursive-element';
 import styles from './styles.module.css';
 import { assetReducer, initialState } from '../../helpers/filters-reducer-configs';
-import { ENUM_ASSET_SENSOR_TYPE, ENUM_ASSET_STATUS, /* ENUM_ASSET_STATUS */ } from '../../../../constants';
+import { ENUM_ASSET_SENSOR_TYPE, ENUM_ASSET_STATUS } from '../../../../constants';
+import energyIcon from '../../../../assets/images/thunderbolt.svg';
+import alertCircle from '../../../../assets/images/alert-circle.svg';
+import energyIconWhite from '../../../../assets/images/thunderbolt-white.svg';
+import alertCircleWhite from '../../../../assets/images/alert-circle-white.svg';
 
 interface TreeProps {
     id: string;
@@ -48,12 +52,22 @@ export const Tree = ({ id }: TreeProps) => {
                     className={`${styles.tree_filters_btns_btn} ${state.assetSensorType ? styles.tree_filters_btns_btn__selected : ''}`}
                     onClick={handleFilterSensorType}
                     >
+                        {
+                            state.assetSensorType ?
+                            <img src={energyIconWhite} alt='energy' /> :
+                            <img src={energyIcon} alt='energy-white' />
+                        }
                         Sensor de energia
                     </button>
                     <button 
                     className={`${styles.tree_filters_btns_btn} ${state.assetStatus ? styles.tree_filters_btns_btn__selected : ''}`}
                     onClick={handleFilterStatus}
                     >
+                        {
+                            state.assetStatus ?
+                            <img src={alertCircleWhite} alt='energy' /> :
+                            <img src={alertCircle} alt='energy' />
+                        }
                         Crítico
                     </button>
                 </div>
